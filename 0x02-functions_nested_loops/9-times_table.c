9a05e16d688bbeb3a7edc0f38e5f7f88c4d7ad6a
Tinyu01 <216135982@edu.vut.ac.za>
2022-10-07 22:33:04 -0700

    edit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,35 +8,33 @@
 
 void times_table(void)
 {
-	int i, j, n;
+	int y, x, prod;
+	prod = 0;
 
-	for (i = 0; i <= 9; i++)
+	for (y = 0; y <= 9; y++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (x = 0; x <= 9; x++)
 		{
-			n = i * j;
-
-			if ((n / 10) == 0)
+			prod = y * x;
+			if (x != 0)
 			{
-				if (j != 0)
+				_putchar(',');
 				_putchar(' ');
-				_putchar(n + '0');
-																				if (j == 9)
-					continue;
-					_putchar(',');
-					_putchar(' ');
-																																		}
-			else
+			}
+			
+			if (prod >= 10)
 			{
-				_putchar((n / 10) + '0');
-				_putchar((n % 10) + '0');
-
-				if (j == 9)
-					continue;
-					_putchar(',');
-					_putchar(' ');
-																																		}
-																		}
+				_putchar((prod / 10) + '0');
+				_putchar((prod % 10) + '0');
+			}
+			else if ((prod < 10) && (x != 0))
+			{
+				_putchar(' ');
+				_putchar((prod % 10) + '0');
+			}
+			else
+				_putchar((prod % 10) + '0');
+			}
 				_putchar('\n');
 		}
 }
