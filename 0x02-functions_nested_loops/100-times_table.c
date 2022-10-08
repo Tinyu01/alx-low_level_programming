93c35e583b77cd26c8d919bc38c6d588d874835d
Tinyu01 <216135982@edu.vut.ac.za>
2022-10-08 01:25:24 -0700

    edit 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-  * print_times_table - print times table up to input
+  * print_times_table - print times table up to input.
   * Description: Writes times table up to imput not exceeding 15
   * @n: size of times table
   * Return: Nothing
