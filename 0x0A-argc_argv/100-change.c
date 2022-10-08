c5a27239f142b65b11b07cf123a74b4de2d4f716
Tinyu01 <216135982@edu.vut.ac.za>
2022-10-08 09:52:20 -0700

    last script editting

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,63 +1,77 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 
 /**
- * main - A proogram that prints the minimum number of coins to
- * make change for an amount of money
- * @argc: The arguements' counter
- * @argv: The argument's values
- * Return: 1 if the number of arguments passed is not exactly
- * or 0 in otherwise
+ * numchecker - verifies if string has only numbers
+ * @s: string to check
+ * Description: return 1 if only numbers in string, 0 if not
+ * Return: 1 if only numbers, 0 if not
  */
 
-int main(int argc, char **argv)
+int numchecker(char *s)
 {
-	int amount, coins = 0;
+	int i;
 
-	if (argc == 2)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		amount = atoi(argv[1]);
-
-		if (amount < 0)
-		{
-			printf("%d\n", 0);
-			return (0);
-		}
-
-		if (amount % 25 >= 0)
-		{
-			coins += amount / 25;
-			amount = amount % 25;
-		}
-	
-		if (amount % 10 >= 0)
-		{
-			coins += amount / 10;
-			amount = amount % 10;
-		}
-
-		if (amount % 5 >= 0)
-		{
-			coins += amount / 5;
-			amount = amount % 5;
-		}
-		
-		if (amount % 2 >= 0)
-		{
-			coins += amount / 2;
-			amount = amount % 2;
-		}
-
-		if (amount % 1 >= 0)
-			coins += amount;
-			printf("%d\n", coins);
+		if (s[i] < '0' || s[i] > '9')
 			return (0);
 	}
-	else
+
+	return (1);
+}
+
+/**
+ * main - find least amount of coins needed for change
+ * @argc: number of parameters entered
+ * @argv: strings entered including file name
+ * Description: prints the minimum number of coins to make change
+ * Return: zero
+ */
+
+int main(int argc, char *argv[])
+{
+	int change, coins;
+	char *input = argv[1];
+
+	change = coins = 0;
+
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+
+	change = atoi(input);
+
+	if (change >= 0 && numchecker(input) == 0)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	if (change < 0 && numchecker(&input[1]) == 0)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	if (change < 0)
+	{
+		change = 0;
+	}
+
+	coins = change / 25;
+	change = change % 25;
+	coins = coins + change / 10;
+	change = change % 10;
+	coins = coins + change / 5;
+	change = change % 5;
+	coins = coins + change / 2;
+	change = change % 2;
+	coins += change;
+	printf("%d\n", coins);
+
+	return (0);
 }
