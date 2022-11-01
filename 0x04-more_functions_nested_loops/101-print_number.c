77f70164be28ab769dc79e42e3e148d0f4b6bb24
Tinyu01 <216135982@edu.vut.ac.za>
2022-11-01 05:54:59 -0700

    scripts corrections

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,40 +1,70 @@
+#include <stdio.h>
 #include "main.h"
-void print_integer(int m);
 
 /**
- * print_number - a function that prints an integer.
- * @n: An input integer
- * Return: Nothing
+ * exponent - x to the power of y
+ * @x: base number
+ * @y: exponent
+ * Description: calcuates x^y
+ * Return: x^y
  */
 
-void print_number(int n)
+int exponent(int x, int y)
 {
-	if (n == 0)
+	int power;
+
+	power = x;
+
+	if (x == 0)
+		return (0);
+
+	if (y == 0)
+		return (1);
+
+	while (y >= 2)
+	{
+		power  = power * x;
+		y--;
 	}
-		_putchar('0');
-	}
-	else if (n < 0)
-	}
-		_putchar('-');
-		print_integer(n * -1);
-	}
-	else
-		print_integer(n);
+	return (power);
 }
 
 /**
- * print_integer - A function to priting n
- * @m: an input unsigned integer
- * Return: Nothing
+ * print_number - print an int using only _putchar
+ * @number: int to be printed by function
+ * Return: nothing
  */
 
-void print_integer(int m)
+void print_number(int number)
 {
-	int i = 1000000000;
+	int size, digit;
+	long counter, sign;
 
-	for (; i >= 1; i /= 10)
-		if (m / i != 0)
-		{
-			_putchar((m / i) % 10 + '0');
-		}
+	sign = 1;
+	digit = 0;
+	size = 1;
+	counter = number;
+
+	if (number < 0)
+	{
+		_putchar('-');
+		sign = -1;
+		counter *= sign;
+	}
+
+	for (; counter >= 10; size++)
+	{
+		counter = counter / 10;
+	}
+
+	counter = sign * (long)number;
+
+	while (size >= 2)
+	{
+		digit = (counter / exponent(10, size - 1));
+		_putchar(digit + '0');
+		counter = counter % exponent(10, size - 1);
+		size--;
+	}
+	_putchar(counter % 10 + '0');
 }
--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,23 +1,25 @@
 #include "main.h"
 
 /**
- * print_diagonal - prints diagonal line
- * @n: length of line
- * Return: nothing
+ * print_diagonal - a function that draws a diagonal line on the terminal.
+ * @n: An input integer
+ * Return: Always 0
  */
 
 void print_diagonal(int n)
 {
-	int x, y;
+	int i = 0, j;
 
-	for (y = 1; y <= n; y++)
+	if (n > 0)
 	{
-		for (x = 1; x < y; x++)
-			_putchar(' ');
-			_putchar('\\');
-	 		_putchar('\n');
+		for (; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+				_putchar(' ');
+			_putchar(92);
+			_putchar('\n');
+		}
 	}
-
-	if (n < 1)
-	_putchar('\n');
+	else
+		_putchar('\n');
 }
