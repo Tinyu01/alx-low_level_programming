51806f059f8bf1c9b40ea8b70c071cd26c31f9bc
Tinyu01 <216135982@edu.vut.ac.za>
2022-11-12 03:55:25 -0800

    102 corrections

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,71 +1,23 @@
 #include "lists.h"
 
 /**
- * free_listp2 - frees a linked list
- * @head: head of a list.
- * Return: no return.
+ * find_listint_loop - A function that finds the loop in a list
+ * @head: A pointer to listint_t structure
+ * Return: The address of the node where the loop start, or NULL
  */
 
-void free_listp2(listp_t **head)
+size_t listint_t find_listint_loop(listint_t **head)
 {
-	listp_t *temp;
-	listp_t *curr;
+	listint_t *one = head, *two = head;
 
-	if (head != NULL)
+	while (one && two && two->next)
 	{
-		curr = *head;
-		while ((temp = curr) != NULL)
+		one = one->next;
+		two = two->next->next;
+		if (one == two)
 		{
-			curr = curr->next;
-			free(temp);
+			return (one);
 		}
-		*head = NULL;
 	}
-}
-
-/**
- * free_listint_safe - frees a linked list.
- * @h: head of a list.
- * Return: size of the list that was freed.
- */
-
-size_t free_listint_safe(listint_t **h)
-{
-	size_t nnodes = 0;
-	listp_t *hptr, *new, *add;
-	listint_t *curr;
-
-	hptr = NULL;
-	while (*h != NULL)
-	{
-		new = malloc(sizeof(listp_t));
-		if (new == NULL)
-			exit(98);
-	
-		new->p = (void *)*h;
-		new->next = hptr;
-		hptr = new;
-
-		add = hptr;
-
-		while (add->next != NULL)
-		{
-			add = add->next;
-			if (*h == add->p)
-			{
-				*h = NULL;
-				free_listp2(&hptr);
-				return (nnodes);
-			}
-		}
-
-		curr = *h;
-		*h = (*h)->next;
-		free(curr);
-		nnodes++;
-	}
-
-	*h = NULL;
-	free_listp2(&hptr);
-	return (nnodes);
+	return (NULL);
 }
